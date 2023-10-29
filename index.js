72190b8d93d4714f74182e73b7365bb314a4dba4
leen-gh <leen.ghaleb.d@gmail.com>
2023-10-30 02:13:11 +0300

    assigment is  done

--- index.js
+++ index.js
@@ -1,1 +1,37 @@
-// Code your solution in this file!
+// Code your solution in this file!\
+function distanceFromHqInBlocks(loco){
+    //let blocks = loco.match(/\d/g)
+    if (loco > 42)
+        return (loco - 42)
+    else
+        return (42 - loco)
+}
+function distanceFromHqInFeet(someValue){
+     let blocks = distanceFromHqInBlocks(someValue)
+     let feet = blocks * 264
+     return feet
+
+}
+function distanceTravelledInFeet(start , end)
+{
+    //let dist = distanceFromHqInFeet(end)
+    let des = Math.abs(end - start) * 264
+    return des
+}
+function calculatesFarePrice(start, destination){
+    let feet = distanceTravelledInFeet(start, destination)
+    feet = parseInt(feet)
+    let price = 0
+    switch(feet){
+        case(feet < 400):
+            return "free sample"
+        case(feet > 400 && feet > 2000):
+            price = (feet - 400 ) * 2
+            return price 
+        case(feet > 2000 && feet < 2500):
+            price = 25
+            return price
+        case (feet > 2500 ):
+            return "cannot travel that far"
+    }
+}
